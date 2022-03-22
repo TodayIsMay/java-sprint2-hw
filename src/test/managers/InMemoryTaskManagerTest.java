075d47432522a9d5e85338d1f5026df02775a915
May <aira.1996@yandex.ru>
2022-03-22 20:29:30 +0300

    убрать тесты в отдельную папку перенести создание задач в EpicTest в отдельный метод

---
+++ src/test/managers/InMemoryTaskManagerTest.java
@@ -0,0 +1,9 @@
+package test.managers;
+
+import managers.InMemoryTaskManager;
+
+class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
+    public InMemoryTaskManagerTest() {
+        super(new InMemoryTaskManager());
+    }
+}
